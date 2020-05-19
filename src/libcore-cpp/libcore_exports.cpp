31b3a2eda1f648664ace90070c8468aceb32925a
Igor Machado <igor.machado@gmail.com>
2020-05-19 12:40:54 -0300

    more info

--- src/libcore-cpp/libcore_exports.cpp
+++ src/libcore-cpp/libcore_exports.cpp
@@ -2,6 +2,6 @@
 // these are the exported functions (to match C library 'core_imports.h')
 // ----------------------------------------------------------------------
 
-// 'mycrypto' method
+// 'mycrypto' method (take from 'libs/libcrypton')
 
-// 'bignum_add' method
+// 'bignum_add' method (take from 'libs/csbiginteger_cpp')
