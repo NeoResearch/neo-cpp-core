7157471a9e0e7a379e56c1660595b090b942657d
Igor Machado <igor.machado@gmail.com>
2020-05-19 21:54:34 -0300

    passing with warnings

--- src/neo3-cpp-core/neopt-common/system/Buffer.hpp
+++ src/neo3-cpp-core/neopt-common/system/Buffer.hpp
@@ -2,7 +2,9 @@
 #define BUFFER_HPP
 
 // system includes
-// none ..
+#include<cstdlib>
+#include<stdio.h>
+#include<string.h>
 
 // core includes
 #include "../system/types.h"
@@ -15,9 +17,14 @@ public:
    // this copy should overwrite data on destination (not append) // TODO: use memcpy over vector data
    static void BlockCopy(const vbyte& src, int srcOffset, vbyte& dst, int dstOffset, int count)
    {
+      // memcpy requires 'restricted' src and dst... assert pointers are different!
+      assert(src.data() != dst.data());
       // srcOffset: The zero-based byte offset into src
 
-      NEOPT_EXCEPTION("Not Implemented BlockCopy");
+      //NEOPT_EXCEPTION("Not Implemented BlockCopy");
+      ::memcpy(dst.data()+dstOffset, src.data(), count);
+
+      std::cout << "WARNING: WE NEED TO TEST BlockCopy()!!!" << std::endl;
    }
 };
 
