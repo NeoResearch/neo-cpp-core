3ab07205903e22b773beb94035839199c283692b
Vitor <vncoelho@gmail.com>
2020-05-22 17:37:11 -0300

    Tested and confirmed. Verification obtained from BigInteger ECField coordinates

--- main-test.cpp
+++ main-test.cpp
@@ -48,8 +48,10 @@ main()
    vbyte vx_32(32, 8); // little-endian value
    vbyte vy_32(32, 9); // little-endian value
 
-   ECFieldElement X{ BigInteger{ vx_32 }, *ECCurve::Secp256r1 };
-   ECFieldElement Y{ BigInteger{ vy_32 }, *ECCurve::Secp256r1 };
+   ECFieldElement X{ BigInteger{ "43563478194357645732161043749214420341576583724275881899443933055754116882936" }, *ECCurve::Secp256r1 };
+   ECFieldElement Y{ BigInteger{ "6800198406926458502571476951697947402787919362010374841430910815761615021861" }, *ECCurve::Secp256r1 };
+   //expected publicKey.EncodePoint(true) { 3, 96, 80, 14, 12, 163, 115, 233, 65, 231, 174, 155, 203, 149, 91, 129, 86, 126, 183, 82, 81, 220, 101, 149, 154, 212, 175, 97, 250, 138, 177, 97, 248, }
+   //expected verification { 12, 33, 3, 96, 80, 14, 12, 163, 115, 233, 65, 231, 174, 155, 203, 149, 91, 129, 86, 126, 183, 82, 81, 220, 101, 149, 154, 212, 175, 97, 250, 138, 177, 97, 248, 11, 65, 138, 107, 30, 117, }
 
    ECPoint ecp{ X, Y, *ECCurve::Secp256r1 };
 
@@ -63,7 +65,7 @@ main()
    //std::cout << vscript << std::endl;
    std::cout << "vscript: ";
    for (auto i : vscript)
-      std::cout << std::hex << ((unsigned int)i) << " ";
+      std::cout << ((unsigned int)i) << " ";
    std::cout << std::endl;
 
    std::cout
