da369fcdd1ca24adf9c9c72bd2a58a2ef50a4bbf
Vitor <vncoelho@gmail.com>
2020-05-28 21:50:25 -0300

    Adding missing $ to var

--- packages/neopt-cpp-lib-js/compile.sh
+++ packages/neopt-cpp-lib-js/compile.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 mkdir -p build/
-EXPORT_NAME='EXPORT_NAME="neopt_raw_lib"'
+$EXPORT_NAME='EXPORT_NAME="neopt_raw_lib"'
 # note that the following command with execute TWO FOLDERS BEFORE => -v $(pwd)/../../:/src
-EMCC_SOURCE_DOCKER=docker run --rm -v $(pwd)/../../:/src trzeci/emscripten em++
-PRE_NODE_LOAD=./packages/neopt-cpp-lib-js/src/prefix-node-require.js
-EXTRA_EXPORTED=EXTRA_EXPORTED_RUNTIME_METHODS='["ccall",  "cwrap", "UTF8ToString", "stringToUTF8"]'
-LIB_JS_TO_BIND=../../packages/neopt-cpp-lib-js/src/neo3-cpp-bindings-js/web-libcore_exports.js
-NEO3_SRC=./src/
-LIBS=./libs/
-LIBS_JS_BUNDLE=./packages/neopt-cpp-lib-js/src/
-MAIN_CPP=./packages/neopt-cpp-lib-js/src/neopt_raw_lib.cpp
-EXTRA_PARAMS=--std=c++17 -g -O3 -Wall -s WASM=1 -s DISABLE_EXCEPTION_CATCHING=1 -s ALLOW_MEMORY_GROWTH=1 -s MODULARIZE=1
+$EMCC_SOURCE_DOCKER=docker run --rm -v $(pwd)/../../:/src trzeci/emscripten em++
+$PRE_NODE_LOAD=./packages/neopt-cpp-lib-js/src/prefix-node-require.js
+$EXTRA_EXPORTED=EXTRA_EXPORTED_RUNTIME_METHODS='["ccall",  "cwrap", "UTF8ToString", "stringToUTF8"]'
+$LIB_JS_TO_BIND=../../packages/neopt-cpp-lib-js/src/neo3-cpp-bindings-js/web-libcore_exports.js
+$NEO3_SRC=./src/
+$LIBS=./libs/
+$LIBS_JS_BUNDLE=./packages/neopt-cpp-lib-js/src/
+$MAIN_CPP=./packages/neopt-cpp-lib-js/src/neopt_raw_lib.cpp
+$EXTRA_PARAMS=--std=c++17 -g -O3 -Wall -s WASM=1 -s DISABLE_EXCEPTION_CATCHING=1 -s ALLOW_MEMORY_GROWTH=1 -s MODULARIZE=1
 
 $(EMCC_SOURCE_DOCKER) $(EXTRA_PARAMS) --pre-js $(PRE_NODE_LOAD) -s $(EXTRA_EXPORTED) -s $(EXPORT_NAME) --js-library $(LIB_JS_TO_BIND) -I$(NEO3_SRC) -I$(LIBS) -I$(LIBS_JS_BUNDLE) $(MAIN_CPP) -o ./packages/neopt-cpp-lib-js/build/neopt_raw_lib.js
 
