67d5fc72e0355d4d852f0ec49c3802c4192b6031
Igor Machado <igor.machado@gmail.com>
2020-05-31 14:14:42 -0300

    auto export working correctly

--- packages/lib-neopt-core-js/export-api-methods.sh
+++ packages/lib-neopt-core-js/export-api-methods.sh
@@ -3,22 +3,29 @@
 # functions with EXPORT_NEO3API prefix (in Neo:: explicit namespace) will be automatically exported
 # we expect these functions to be string -> string, so if signature is not this, export manually on .cpp file
 
-echo "Listing EXPORT_NEO3API methods (remember to include Neo:: scope)"
+echo "Listing EXPORT_NEO3API methods (remember to include Neo:: scope and auto-export only string->string methods)"
 echo ""
 (cd ../../src && grep -rh "EXPORT_NEO3API" . | grep "Neo::")
 
 echo ""
-OUTFILE=lib-neopt-core-js.tmp.cpp
-echo "Adding these to $OUTFILE file"
-cp   src/lib-neopt-core-js.cpp   $OUTFILE
+BASEFILE=src/lib-neopt-core-js.base.cpp
+OUTFILE=src/lib-neopt-core-js.cpp
+#
+BASEFILE_JS=src/index.base.js
+OUTFILE_JS=src/index.js
+#
+echo "Adding these to '$OUTFILE' file"
+echo "// AUTOMATICALLY GENERATED FILE - DO NOT EDIT THIS" > $OUTFILE
+echo "// AUTOMATICALLY GENERATED FILE - DO NOT EDIT THIS" > $OUTFILE_JS
+cat   $BASEFILE  >>  $OUTFILE
+cat   $BASEFILE_JS  >>  $OUTFILE_JS
 #
 for func in `(cd ../../src && grep -rh "EXPORT_NEO3API" . | grep "Neo::" | cut -d '(' -f1 )`; do
     #
-    func2=${func/EXPORT_NEO3API_}  # remove EXPORT_NEO3API_
-    #echo $func2
-    func3=${func2/Neo::/Neo3::}
-    #echo $func3
-    func4=$(echo $func3 | sed "s/::/\_/g")
+    func1=${func/Neo::/Neo3::}
+    func2=${func1/EXPORT_NEO3API_}  # remove EXPORT_NEO3API_
+    #
+    func4=$(echo $func2 | sed "s/::/\_/g")
     #echo $func4
     # ======================
     # writing to output file
@@ -30,6 +37,13 @@ for func in `(cd ../../src && grep -rh "EXPORT_NEO3API" . | grep "Neo::" | cut -
     echo "EMSCRIPTEN_KEEPALIVE" >> $OUTFILE
     echo "extern \"C\" const char*" >> $OUTFILE
     echo "$func4(const char* cs_in) {" >> $OUTFILE
-    echo "return $func2(std::string(cs_in)).c_str();" >> $OUTFILE
+    echo "return $func(std::string(cs_in)).c_str();" >> $OUTFILE
     echo "}" >> $OUTFILE
+    #
+    # js part
+    #
+    echo "" >> $OUTFILE_JS
+    echo "// this method was automatically imported" >> $OUTFILE_JS
+    echo "export const $func4 =" >> $OUTFILE_JS
+    echo "   mymodule.cwrap('$func4', 'string', ['string']);" >> $OUTFILE_JS
 done
